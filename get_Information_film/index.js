d17f6262f887c608c194ab4f06808b728495dd20
Tovmas Petrosyan <tompetrosyan30@gmail.com>
2023-11-14 20:07:07 +0400

    Create Img element

--- get_Information_film/index.js
+++ get_Information_film/index.js
@@ -3,3 +3,9 @@ const response = await fetch(apiUrl);
 const data = await response.json();
 return data;
 }
+
+function createImageElement(src){
+    let img = document.createElement("img");
+    img.src = src;
+    return img;
+}
