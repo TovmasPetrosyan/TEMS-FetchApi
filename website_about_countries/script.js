f71908ec02f44e1381d2f6201fe75554c3873509
Meri Mkrtchyan <meri.mkrtchyan1999@gmail.com>
2023-11-16 21:13:47 +0400

    add flag in index.html

--- website_about_countries/script.js
+++ website_about_countries/script.js
@@ -3,7 +3,7 @@ let button = document.querySelector(".button")
 let serchValue = ""
 let countryArray = []
 
-fetch("https://restcountries.com/v3.1/all")
+fetch("https://restcountries.com/v3.1/all?fields=name")
     .then(reponse => reponse.json())
     .then(countries => {
        
@@ -13,24 +13,53 @@ fetch("https://restcountries.com/v3.1/all")
             country.innerText = `${countries[objCountry].name.common}`
             selectElement.append(country)
             countryArray.push(countries[objCountry].name.common)
-        }
-    })
-
-    fetch("https://restcountries.com/v3.1/all")
-    .then(reponse => reponse.json())
-    .then(countries => {
-        button.addEventListener("click", () =>{
-            serchValue = search.value
-            if (countryArray.includes(serchValue)){
-                console.log("true")
-            }else {
-                console.log("false")
-            }
-            
-        })
+        }   
 })
 
-console.log(countryArray)
+button.addEventListener("click", () =>{
+    serchValue = search.value
+
+        if (!serchValue){
+            serchValue = document.querySelector("select").value  
+        }
+
+        if (countryArray.includes(serchValue)){
+            countryInformation(serchValue)
+        } else {
+            console.log(Error)
+        }
+})
+
+function countryInformation(serchValue){
+    fetch(`https://restcountries.com/v3.1/name/${serchValue}`)
+        .then(reponse => reponse.json())
+        .then(countries => {
+
+            let flag = countries[0].flags.png 
+            let name = countries[0].name.official
+            let getcapital =  countries[0].capital
+            let languages = countries[0].languages
+            let population = countries[0].population
+            let area = countries[0].area
+
+            let image = document.querySelector(".imageClass") 
+            let imagePng = document.createElement("img")
+            imagePng.setAttribute("src", flag)
+            image.append(imagePng)
+            imagePng.setAttribute("class", "flagClass")
+
+        })
+
+}
+
+
+
+
+
+
+
+
+
 
 
 
