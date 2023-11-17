97188ccd194023ef71158805a533a487b5f3fb85
Meri Mkrtchyan <meri.mkrtchyan1999@gmail.com>
2023-11-17 22:25:21 +0400

    add change in script.js added a new divs to the div countryInformation in the index.html

--- website_about_countries/script.js
+++ website_about_countries/script.js
@@ -1,15 +1,16 @@
-let search = document.querySelector(".search")
-let button = document.querySelector(".button")
-let select = document.querySelector("select")
-let countryArray = []
-let existingErrorDiv = false
+const search = document.querySelector(".search")
+const button = document.querySelector(".button")
+const select = document.querySelector("select")
+const countryIformation = document.querySelector(".countryIformation");
+const countryArray = []
+
 
 fetch("https://restcountries.com/v3.1/all?fields=name")
     .then(reponse => reponse.json())
     .then(countries => {
         for (let objCountry in countries) {
-            let selectElement = document.querySelector("select")
-            let country = document.createElement("option")
+            const selectElement = document.querySelector("select")
+            const country = document.createElement("option")
             country.innerText = `${countries[objCountry].name.common}`
             selectElement.append(country)
             countryArray.push(countries[objCountry].name.common)
@@ -17,45 +18,22 @@ fetch("https://restcountries.com/v3.1/all?fields=name")
 })
 
 select.addEventListener("mouseup", () =>{
-    let serchValue = document.querySelector("select").value
-    let errorDiv = document.querySelector(".error")
-    if (errorDiv) {
-        errorDiv.remove();
-    } 
-    countryInformation(serchValue)
+    countryInformation(document.querySelector("select").value)
 })
 
 button.addEventListener("click", () =>{
-    serchValue = search.value
 
-    if (!countryArray.includes(serchValue)){
-
-        let imageClass = document.querySelector(".imageClass");
-        let countryName = document.querySelector(".countryName");
-        let capital = document.querySelector(".capital");
-        let languages = document.querySelector(".languages");
-        let population = document.querySelector(".population");
-        let area = document.querySelector(".area");
-        clear(imageClass)
-        clear(countryName)
-        clear(capital)
-        clear(languages)
-        clear(population)
-        clear(area)
-
-        let countryIformation = document.querySelector(".countryIformation");
-        let newDiv = document.createElement("div")
+    
+    if (!countryArray.includes(search.value)){
+        clear(countryIformation)
+        const newDiv = document.createElement("div")
         newDiv.classList.add("error")
-        let error = document.createElement("img")
+        const error = document.createElement("img")
         error.setAttribute("src", "./error.png")
         newDiv.append(error)
-        let existingErrorDiv = document.querySelector(".error");
-        if (existingErrorDiv) {
-            existingErrorDiv.remove();
-        }   
         countryIformation.append(newDiv)
-    }else{
-        countryInformation(serchValue)
+    }else {
+        countryInformation(search.value)
     }
 })
 
@@ -64,53 +42,61 @@ function countryInformation(serchValue){
         .then(reponse => reponse.json())
         .then(countries => {
 
-            let flag = countries[0].flags.png 
-            let name = countries[0].name.official
-            let getcapital =  countries[0].capital
-            let languages = countries[0].languages
-            let population = countries[0].population
-            let area = countries[0].area
-            
+            const flag = countries[0].flags.png 
+            const name = countries[0].name.official
+            const getcapital =  countries[0].capital
+            const languagesAll = countries[0].languages
+            const pop = countries[0].population
+            const areaCountre = countries[0].area
 
-            let image = document.querySelector(".imageClass") 
-            clear(image)
-            let imagePng = document.createElement("img")
+            clear(countryIformation)
+
+            const imageClass = document.createElement("div")
+            imageClass.classList.add("imageClass")
+            const imagePng = document.createElement("img")
             imagePng.setAttribute("src", flag)
-            image.append(imagePng)
             imagePng.setAttribute("class", "flagClass")
+            imageClass.append(imagePng)
+            countryIformation.append(imageClass)
 
-            let countryName = document.querySelector(".countryName")
-            clear(countryName)
-            let fullName = document.createElement("h2")
+
+            const countryName = document.createElement("div")
+            countryName.classList.add("countryName")
+            const fullName = document.createElement("h2")
             fullName.innerText = name
             countryName.append(fullName)
+            countryIformation.append(countryName)
 
-            let capital = document.querySelector(".capital")
-            clear(capital)
-            let capitalName = document.createElement("p")
+            const capital = document.createElement("div")
+            capital.classList.add("capital")
+            const capitalName = document.createElement("p")
             capitalName.innerText = `Capital:  ${getcapital}`
             capital.append(capitalName)
+            countryIformation.append(capital)
 
-            if (languages){
-                let language =  Object.values(languages).join(", ")
-                let languagesBox = document.querySelector(".languages")
-                clear(languagesBox)
-                let languagesName = document.createElement("p")
-                languagesName.innerText = `Oficial languages:  ${language}`
-                languagesBox.append(languagesName)
-             }
+            if (languagesAll){
+                const languageBox =  Object.values(languagesAll).join(", ")
+                const languages = document.createElement("div")
+                languages.classList.add("languages")
+                const languagesName = document.createElement("p")
+                languagesName.innerText = `Oficial languages:  ${languageBox}`
+                languages.append(languagesName)
+                countryIformation.append(languages)
+            }
 
-            let populationClass = document.querySelector(".population")
-            clear(populationClass)
-            let populationName = document.createElement("p")
-            populationName.innerText = `Population:  ${population}`
-            populationClass.append(populationName)
+            const population = document.createElement("div")
+            population.classList.add("population")
+            const populationName = document.createElement("p")
+            populationName.innerText = `Population:  ${pop}`
+            population.append(populationName)
+            countryIformation.append(population)
 
-            let areaClass = document.querySelector(".area")
-            clear(areaClass)
-            let areaName = document.createElement("p")
-            areaName.innerText = `Nationality:  ${area}`
-            areaClass.append(areaName) 
+            const area = document.createElement("div")
+            area.classList.add("area")
+            const areaName = document.createElement("p")
+            areaName.innerText = `Nationality:  ${areaCountre}`
+            area.append(areaName)
+            countryIformation.append(area)
         })
 }
 
@@ -119,22 +105,3 @@ function clear(element) {
         element.removeChild(element.firstChild);
     }
 }
-
-function clearAll(...elements) {
-    for(let element = 0 ; element < elements.length ; element++) {
-        while (element.firstChild) {
-            element.removeChild(element.firstChild);
-        }
-    }
-    
-}
-
-
-
-
-
-
-
-
-
-
