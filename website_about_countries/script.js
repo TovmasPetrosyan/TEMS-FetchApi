52b7f3db418ad81a956f67d3866cee510bf41379
Meri Mkrtchyan <meri.mkrtchyan1999@gmail.com>
2023-11-17 16:46:36 +0400

    add code for error

--- website_about_countries/script.js
+++ website_about_countries/script.js
@@ -2,11 +2,11 @@ let search = document.querySelector(".search")
 let button = document.querySelector(".button")
 let select = document.querySelector("select")
 let countryArray = []
+let existingErrorDiv = false
 
 fetch("https://restcountries.com/v3.1/all?fields=name")
     .then(reponse => reponse.json())
     .then(countries => {
-       
         for (let objCountry in countries) {
             let selectElement = document.querySelector("select")
             let country = document.createElement("option")
@@ -18,16 +18,44 @@ fetch("https://restcountries.com/v3.1/all?fields=name")
 
 select.addEventListener("mouseup", () =>{
     let serchValue = document.querySelector("select").value
+    let errorDiv = document.querySelector(".error")
+    if (errorDiv) {
+        errorDiv.remove();
+    } 
     countryInformation(serchValue)
 })
 
 button.addEventListener("click", () =>{
     serchValue = search.value
-    
-    if (countryArray.includes(serchValue)){
-            countryInformation(serchValue)
-    } else {
-            console.log(Error)
+
+    if (!countryArray.includes(serchValue)){
+
+        let imageClass = document.querySelector(".imageClass");
+        let countryName = document.querySelector(".countryName");
+        let capital = document.querySelector(".capital");
+        let languages = document.querySelector(".languages");
+        let population = document.querySelector(".population");
+        let area = document.querySelector(".area");
+        clear(imageClass)
+        clear(countryName)
+        clear(capital)
+        clear(languages)
+        clear(population)
+        clear(area)
+
+        let countryIformation = document.querySelector(".countryIformation");
+        let newDiv = document.createElement("div")
+        newDiv.classList.add("error")
+        let error = document.createElement("img")
+        error.setAttribute("src", "/error.png")
+        newDiv.append(error)
+        let existingErrorDiv = document.querySelector(".error");
+        if (existingErrorDiv) {
+            existingErrorDiv.remove();
+        }   
+        countryIformation.append(newDiv)
+    }else{
+        countryInformation(serchValue)
     }
 })
 
@@ -42,6 +70,7 @@ function countryInformation(serchValue){
             let languages = countries[0].languages
             let population = countries[0].population
             let area = countries[0].area
+            
 
             let image = document.querySelector(".imageClass") 
             clear(image)
@@ -81,10 +110,8 @@ function countryInformation(serchValue){
             clear(areaClass)
             let areaName = document.createElement("p")
             areaName.innerText = `Nationality  ${area}`
-            areaClass.append(areaName)  
-
+            areaClass.append(areaName) 
         })
-
 }
 
 function clear(element) {
@@ -93,10 +120,14 @@ function clear(element) {
     }
 }
 
-
-
-
-
+function clearAll(...elements) {
+    for(let element = 0 ; element < elements.length ; element++) {
+        while (element.firstChild) {
+            element.removeChild(element.firstChild);
+        }
+    }
+    
+}
 
 
 
