76b7d156c6cfab4fb88a7a23a1c0bb242137d527
suren <tadevosyansuren07@gmail.com>
2023-11-17 12:16:16 +0400

    creating feat. second select element which controls the quantity of fetched elements

--- getDogAPIs/getDogImgs.js
+++ getDogAPIs/getDogImgs.js
@@ -1,20 +1,27 @@
 let selectElement = document.getElementById("selectDog");
 let container = document.getElementById("dogImages");
+let modalElem = document.getElementById("imageModal");
+let number = document.getElementById("quantity");
 
 function createImageElement(src) {
   let img = document.createElement("img");
   img.src = src;
   return img;
 }
+
 function clearContainer() {
   container.textContent = "";
 }
-function fetchImages(selectedBreed) {
-  return fetch(`https://dog.ceo/api/breed/${selectedBreed}/images`)
+
+function fetchImages(selectedBreed, length) {
+  return fetch(
+    `https://dog.ceo/api/breed/${selectedBreed}/images/random/${length}`
+  )
     .then((res) => res.json())
     .then((data) => {
       data.message.forEach((imgURL) => {
         let img = createImageElement(imgURL);
+        img.classList.add("dog-image");
         img.onerror = () => {
           clearContainer();
           img.src = "404dog.jpg";
@@ -27,6 +34,7 @@ function fetchImages(selectedBreed) {
       console.error("Error fetching images:", err);
     });
 }
+
 fetch(`https://dog.ceo/api/breeds/list/all`)
   .then((res) => res.json())
   .then((data) => {
@@ -37,9 +45,41 @@ fetch(`https://dog.ceo/api/breeds/list/all`)
       selectElement.appendChild(option);
     });
   });
+
 selectElement.addEventListener("change", () => {
   let selectedBreed =
     selectElement.value.charAt(0).toLowerCase() + selectElement.value.slice(1);
   clearContainer();
-  fetchImages(selectedBreed);
+  number.innerHTML = "";
+  fetch(`https://dog.ceo/api/breed/${selectedBreed}/images`)
+    .then((res) => res.json())
+    .then((data) => {
+      data.message.forEach((imgURL, index) => {
+        let img = createImageElement(imgURL);
+        img.onerror = () => {
+          img.src = "404dog.jpg";
+          img.classList.add("dog-image");
+
+          container.appendChild(img);
+          console.log("Image not found");
+        };
+        container.appendChild(img);
+        let option = document.createElement("option");
+        option.innerText = index + 1;
+        number.appendChild(option);
+      });
+    })
+    .catch((err) => {
+      console.error("Error fetching breed images:", err);
+    });
 });
+
+number.addEventListener("change", () => {
+  let selectedBreed =
+    selectElement.value.charAt(0).toLowerCase() + selectElement.value.slice(1);
+  clearContainer();
+  let length = number.value;
+  fetchImages(selectedBreed, length);
+});
+
+
